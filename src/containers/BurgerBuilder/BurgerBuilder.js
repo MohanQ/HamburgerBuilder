1fdb9bf496413e4fde004936e21338db2655f210
HorvathTamas <horvath.tamas.norbert93@gmail.com>
2018-10-30 00:58:15 +0100

    Add Router for the App and create Checkout/ContactData pages.

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,29 +87,20 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Horvath Tamas',
-                adress: {
-                    street: 'Test street',
-                    zipCode: '45652',
-                    Country: 'Hungary'
-                },
-                email: 'testelek@test.com'
-            },
-            deliveryMethod: 'fastest'
+        const queryParams = [];
+
+        for (let i in this.state.ingredients) {
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
 
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        queryParams.push('price=' + this.state.totalPrice);
+
+        const queryString = queryParams.join('&');
+
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString
+        });
     }
 
     render () {
