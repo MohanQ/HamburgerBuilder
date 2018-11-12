9d3a08b1fc37c15a3b188494734f73103407e5e1
HorvathTamas <horvath.tamas.norbert93@gmail.com>
2018-11-13 00:04:48 +0100

    Guarding routes and displaying user specific orders only

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,7 @@ const navigationItems = (props) => (
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         {props.isAuthenticated ? <NavigationItem link="/orders">Orders</NavigationItem> : null}
         {!props.isAuthenticated 
-            ? <NavigationItem link="/auth">Authenticated</NavigationItem>
+            ? <NavigationItem link="/auth">Authenticate</NavigationItem>
             : <NavigationItem link="/logout">Logout</NavigationItem>}
     </ul>
 );
